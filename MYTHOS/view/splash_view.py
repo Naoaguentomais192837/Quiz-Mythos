1e826ef4713b8efa3a04b3fb1945b88688f14764
Naoaguentomais192837 <gabizinha222229283@gmail.com>
2025-10-07 19:29:52 -0300

    Add files via upload

---
+++ MYTHOS/view/splash_view.py
@@ -0,0 +1,33 @@
+import flet as ft
+import asyncio
+
+class SplashView(ft.View):
+    def __init__(self, page, go_to):
+        super().__init__()
+        self.page = page
+        self.go_to = go_to
+        self.expand = True
+
+        background = ft.Image(src="background1.png", fit=ft.ImageFit.COVER, expand=True)
+        logo = ft.Image(src="anubis_logo.png", width=220, height=220)
+
+        content = ft.Column(
+            [logo],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        )
+
+        self.controls = [
+            ft.Stack([
+                ft.Container(content=background, expand=True),
+                ft.Container(content=content, alignment=ft.alignment.center, expand=True)
+            ], expand=True)
+        ]
+
+        page.run_task(self._delayed_nav)
+
+    async def _delayed_nav(self):
+        await asyncio.sleep(2)
+        self.go_to("start")
+
+
